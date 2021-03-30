df7d0ba531c5acd3e8c42fda7aff8ec9995642f2
ilarioni <ilia.meshvildishvili@gmail.com>
2021-03-30 16:43:10 +0200

    changing particle speed

--- script.js
+++ script.js
@@ -77,12 +77,14 @@ myImage.addEventListener('load', function(){
     }
     init();
     function animate(){
-        ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+        //ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
         ctx.globalAlpha = 0.05;
         ctx.fillStyle = 'rgb(0, 0, 0)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 0.2;
         for (let i = 0; i < particlesArray.length; i++){
             particlesArray[i].update();
+            ctx.globalAlpha = particlesArray[i].speed;
             particlesArray[i].draw();
         }
         requestAnimationFrame(animate);
