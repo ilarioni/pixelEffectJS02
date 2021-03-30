7548d0e349d80258d87676a2dcc8f93014dcbc8f
ilarioni <ilia.meshvildishvili@gmail.com>
2021-03-30 16:32:52 +0200

    nice speed of pixel rain

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ myImage.addEventListener('load', function(){
         }
         mappedImage.push(row);
     }
+    console.log(mappedImage);
 
     function calculateRelativeBrightness(red, green, blue){
         return Math.sqrt(
@@ -45,11 +46,18 @@ myImage.addEventListener('load', function(){
             this.x = Math.random() * canvas.width;
             this.y = 0;
             this.speed = 0;
-            this.velocity = Math.random() * 3.5;
+            this.velocity = Math.random() * 0.5;
             this.size = Math.random() * 1.5 + 1;
+            this.position1 = Math.floor(this.y);
+            this.position2 = Math.floor(this.x);
         }
         update(){
-            this.y += this.velocity;
+            this.position1 = Math.floor(this.y);
+            this.position2 = Math.floor(this.x);
+            this.speed = mappedImage[this.position1][this.position2][0];
+            let movement = (2.5 - this.speed) + this.velocity;
+
+            this.y += movement;
             if (this.y >= canvas.height){
                 this.y = 0;
                 this.x = Math.random() * canvas.width;
